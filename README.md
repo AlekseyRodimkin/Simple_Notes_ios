1290d5b51ae19871f636473fbc34a24813cc084f
aleksei_rodimkin <alexeyrodimkin@gmail.com>
2025-10-24 15:50:36 +0300

    Added search

--- README.md
+++ README.md
@@ -9,14 +9,17 @@
 ### Заметки добавлены
 <img src="https://github.com/AlekseyRodimkin/Simple_Notes_ios/blob/main/Readme_images/new.png" style="width:30%;">
 
-### Удаляем заметку свайпом
-<img src="https://github.com/AlekseyRodimkin/Simple_Notes_ios/blob/main/Readme_images/swipe.png" style="width:30%;">
-
 ### Редактируем заметку
 <img src="https://github.com/AlekseyRodimkin/Simple_Notes_ios/blob/main/Readme_images/writing.png" style="width:30%;">
 
 ### Заметка изменена
 <img src="https://github.com/AlekseyRodimkin/Simple_Notes_ios/blob/main/Readme_images/changed.png" style="width:30%;">
+
+### Поиск заметок
+<img src="https://github.com/AlekseyRodimkin/Simple_Notes_ios/blob/main/Readme_images/search.png" style="width:30%;">
+
+### Удаляем заметку свайпом
+<img src="https://github.com/AlekseyRodimkin/Simple_Notes_ios/blob/main/Readme_images/swipe.png" style="width:30%;">
 
 ### Удаляем несколько заметок
 <img src="https://github.com/AlekseyRodimkin/Simple_Notes_ios/blob/main/Readme_images/option.png" style="width:30%;">
--- notes_ios_1.0/ContentView.swift
+++ notes_ios_1.0/ContentView.swift
@@ -6,21 +6,39 @@ struct ContentView: View {
     @Query(sort: \Note.timestamp, order: .reverse) private var notes: [Note]
 
     @State private var viewModel: NoteListViewModel?
+    @State private var searchText = ""
+
+    private var filteredNotes: [Note] {
+        if searchText.isEmpty {
+            return notes
+        } else {
+            return notes.filter { note in
+                note.title.localizedCaseInsensitiveContains(searchText)
+                || note.content.localizedCaseInsensitiveContains(searchText)
+                || formattedDate(note.timestamp).localizedCaseInsensitiveContains(searchText)
+                || formattedDate(note.updatedAt).localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(notes) { note in
-                    NavigationLink(destination: NoteDetailView(note: note)) {
-                        Text(note.title)
-                            .font(.headline)
-                            .lineLimit(1)
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(filteredNotes) { note in
+                        NavigationLink(destination: NoteDetailView(note: note)) {
+                            Text(note.title)
+                                .font(.headline)
+                                .lineLimit(1)
+                        }
+                    }
+                    .onDelete { offsets in
+                        viewModel?.deleteNotes(filteredNotes, at: offsets)
                     }
                 }
-                .onDelete { offsets in
-                    viewModel?.deleteNotes(notes, at: offsets)
-                }
             }
+            .navigationTitle("Заметки")
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
@@ -33,13 +51,18 @@ struct ContentView: View {
                     }
                 }
             }
-            .navigationTitle("Мои заметки")
-        } detail: {
-            Text("Select an item")
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Поиск")
         }
         .onAppear {
             viewModel = NoteListViewModel(modelContext: modelContext)
         }
+    }
+
+    private func formattedDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter.string(from: date)
     }
 }
 
